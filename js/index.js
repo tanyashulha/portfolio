13bd334525cdd7cb7d1565fb09e219dcf0d19ad6
tanyashulha <tsulga6@gmail.com>
2019-10-19 13:04:32 +0300

    feat: implement education slide

--- js/index.js
+++ js/index.js
@@ -1,12 +1,19 @@
 "use strict";
 
-var swiper = new Swiper('#main-slider', {
-    direction: 'vertical',
-    slidesPerView: 1,
-    mousewheel: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-      dynamicBullets: true,
-    },
+
+//Main slider
+const mainSlider = new Swiper('#main-slider', {
+  direction: 'vertical',
+  slidesPerView: 1,
+  mousewheel: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
 });
+
+document.getElementById('click-block').onclick = function() {
+  document.getElementById('click-block').classList.toggle('active');
+  document.getElementById('result').classList.toggle('slide-descr-active');
+}
